1b0ee14d801c03c9e54a7950c51bc4941d99da1e
Emmett Hitz <emmetthitz@gmail.com>
2023-01-12 22:38:55 +0000

    use base64 -d instead of --decode for better base64 version compliance

--- modules/agent-policy/scripts/create-update-script.sh
+++ modules/agent-policy/scripts/create-update-script.sh
@@ -27,11 +27,11 @@ set -x # debug mode
 PROJECT_ID="$1"
 POLICY_ID="$2"
 DESCRIPTION="$3"
-AGENT_RULES_JSON="$(echo "$4" | base64 --decode)"
-GROUP_LABELS_JSON="$(echo "$5" | base64 --decode)"
-OS_TYPES_JSON="$(echo "$6" | base64 --decode)"
-ZONES_JSON="$(echo "$7" | base64 --decode)"
-INSTANCES_JSON="$(echo "$8" | base64 --decode)"
+AGENT_RULES_JSON="$(echo "$4" | base64 -d)"
+GROUP_LABELS_JSON="$(echo "$5" | base64 -d)"
+OS_TYPES_JSON="$(echo "$6" | base64 -d)"
+ZONES_JSON="$(echo "$7" | base64 -d)"
+INSTANCES_JSON="$(echo "$8" | base64 -d)"
 
 
 # include functions to build gcloud command
