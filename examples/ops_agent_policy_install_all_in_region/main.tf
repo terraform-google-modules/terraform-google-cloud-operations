4607369cfbfa4c953d4e5990431ad2d679ba3227
Henrique Matulis <hmatulis@google.com>
2024-05-15 17:27:12 -0400

    Add example to install all in region

---
+++ examples/ops_agent_policy_install_all_in_region/main.tf
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+provider "google" {
+  project     = var.project_id
+}
+
+data "google_compute_zones" "available" {
+  region = var.region
+}
+
+module "ops_agent_policy" {
+  for_each = toset(data.google_compute_zones.available.names)
+  source      = "./../../modules/ops-agent-policy"
+  assignment_id = "ops_agent_policy_all_in_${replace(each.key, "-", "_")}"
+  zone = each.key
+  instance_filter = {all=true}
+}
